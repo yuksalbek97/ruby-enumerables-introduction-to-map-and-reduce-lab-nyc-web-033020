caa2e8e62d4b89c98f812e7c69a15cde842efc6e
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-03-27 01:03:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,16 +62,6 @@ end
 
 
 
-def reduce_to_any_true(source_array)
-counter = 0 
-while counter < source_array.size do
- if source_array[counter] == false
-   return false
-end
- counter += 1
-return false
-end
-end
 
 
 def reduce_to_any_true(source_array)
