045f014172c8b74419d97ba0694d1d5969f0f4ca
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-03-27 00:32:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,68 @@
-# My Code here....
+# My Code here...
+nums = [1, 2, 3, -9]
+final_array = []
+  counter = 0 
+  while counter < nums.size do
+   final_array.push(nums[counter] * -1)
+  end
+final_array
+end
+
+ def map_to_no_change(dune)
+  final_array = []
+  counter = 0 
+  while counter < dune.size do
+   final_array.push(dune[counter])
+  end
+final_array
+end
+
+ def map_to_double(nums)
+  final_array = []
+  counter = 0 
+  while counter < nums.size do
+   final_array.push(nums[counter] * 2)
+  end
+final_array
+end
+
+  def map_to_double(nums)
+  final_array = []
+  counter = 0 
+  while counter < nums.size do
+   final_array.push(nums[counter] ** 2)
+  end
+final_array
+end
+
+def reduce_to_total(source_array,starting_point = 0)
+total = starting_point
+counter = 0
+while counter < source_array.size do
+  total += source_array[counter]
+  counter += 1
+end
+total
+end
+
+def reduce_to_all_true(source_array)
+counter = 0 
+while counter < source_array.size do
+return false if source_array[counter] == false
+end
+return true
+end
+end
+
+def reduce_to_all_true(source_array)
+counter = 0 
+while counter < source_array.size do
+return true if source_array[counter] == true
+end
+return false
+end
+end
+
+
+
+
--- spec/map_and_reduce_spec.rb
+++ spec/map_and_reduce_spec.rb
@@ -6,26 +6,27 @@ describe 'my own map-like methods' do
       expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, 9])
     end
   end
-
+end
+  
   describe "map_to_no_change returns an array with the original values" do
     it "transforms correctly" do
       dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
       expect(map_to_no_change(dune)).to eq(dune)
     end
   end
-
+  
   describe "map_to_double returns an array with the original values multiplied by 2" do
     it "transforms correctly" do
       expect(map_to_double([1, 2, 3, -9])).to eq([2, 4, 6, -18])
     end
   end
-
+  
   describe "map_to_square returns an array with the original values squared" do
     it "transforms correctly" do
       expect(map_to_square([1, 2, 3, -9])).to eq([1, 4, 9, 81])
     end
   end
-end
+
 
 describe 'my own reduce-like methods' do
   describe "reduce_to_total returns a running total when not given a starting point" do
@@ -34,6 +35,7 @@ describe 'my own reduce-like methods' do
       expect(reduce_to_total(source_array)).to eq(6)
     end
   end
+end
 
   describe "reduce_to_total returns a running total when given a starting point" do
     it "reduces correctly" do
@@ -49,6 +51,8 @@ describe 'my own reduce-like methods' do
       expect(reduce_to_all_true(source_array)).to be_truthy
     end
   end
+
+d
 
   describe "reduce_to_all_true returns false when any value is false" do
     it "reduces correctly" do
@@ -69,5 +73,6 @@ describe 'my own reduce-like methods' do
       source_array = [ false, nil, nil, nil]
       expect(reduce_to_any_true(source_array)).to eq(false)
     end
-  end
+  
 end
+
